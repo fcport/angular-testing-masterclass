2313a47d2e45d33b6772eff0715fa1fb5e59a13f
Lilililim <federico.casadei.portfolio@gmail.com>
2023-01-02 14:50:56 +0100

    Starting point lesson 03

---
+++ src/app/pipes/reverse.pipe.spec.ts
@@ -0,0 +1,10 @@
+import { ReversePipe } from './reverse.pipe';
+
+describe('ReversePipe', () => {
+  
+  it('create an instance', () => {
+    const pipe = new ReversePipe();
+    expect(pipe).toBeTruthy();
+  });
+
+});
